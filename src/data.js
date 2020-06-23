4b41404c99aadcb1a7464e798873b901ff37c239
marcelacoronel <marcelacoronelp@gmail.com>
2020-06-23 12:24:50 -0500

    Cambios estructura html marce 1

--- src/data.js
+++ src/data.js
@@ -1,9 +1,15 @@
 // estas funciones son de ejemplo
 
-export const example = () => {
+ export const example = () => {
+
+
+
+  
   return 'example';
 };
 
 export const anotherExample = () => {
   return 'OMG';
 };
+
+
