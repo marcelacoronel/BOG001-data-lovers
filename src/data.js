7333f3e0e9a72c3bd02408832c5349ef3005d9cc
karlaguaita <66104641+karlaguaita@users.noreply.github.com>
2020-06-24 21:55:06 -0400

    creacion de rama

--- src/data.js
+++ src/data.js
@@ -1,15 +1,10 @@
-// estas funciones son de ejemplo
 
- export const example = () => {
+export const buscarPokemon = (valor,data) => {
+  //console.log(valor)
+  const arreglo = data.pokemon // Guarde la data del arreglo de pokemon
 
 
-
-  
-  return 'example';
+  // Se define la constante pokemon para el resultado de la busqueda y luego se busca en el arreglo con la propiedad find
+  const pokemon = arreglo.find(pokemon => pokemon.name.toLowerCase() == valor.toLowerCase() || pokemon.id == valor)
+  return pokemon
 };
-
-export const anotherExample = () => {
-  return 'OMG';
-};
-
-
--- src/main.js
+++ src/main.js
@@ -1,25 +1,48 @@
-import { example } from './data.js';
-// import { verDatos } from './data.js';
-// import data from './data/lol/lol.js';
+import { buscarPokemon } from './data.js';
+// import { verDatos } from './data.js'
+
+// import data from './data/pokemon/pokemon.js'
 import data from './data/pokemon/pokemon.js';
-// import data from './data/rickandmorty/rickandmorty.js';
-
-// console.log(example,data);
-console.log(example,data);
 
 
-document.getElementById('boton').addEventListener('click',iniciar);
+//declaracion de la variable
+let nombreNumeroPokemon = document.getElementById('nombreNumero')
+//declaracion del evento
+document.getElementById('boton').addEventListener('click',function(){llenarFrontPokemon(nombreNumeroPokemon.value)});
 
-function iniciar(){
-const arreglos = data;
-let datos = arreglos.map( arreglo => {
-    return {
-        id: arreglo.id,
-        name: arreglo.name
+// Evento cuando se presiona el enter en el input del nombre del pokemon
+nombreNumeroPokemon.addEventListener("keyup", function(event) {
+    // Numero 13 es el "enter" del teclado
+    if (event.keyCode === 13) {
+      // Cancela accion por defecto para ejecutar este enter
+      event.preventDefault();
+      // accion que se ejecuta cuando leda enter
+      document.getElementById("boton").click();
     }
-})
-console.log("datos=>",datos);
+  });
 
+// function iniciar(){
+//     const arreglos = data;
+//     let datos = arreglos.map( arreglo => {
+//         return {
+//             id: arreglo.id,
+//             name: arreglo.name
+//         }
+//     })
+//     console.log("datos=>",datos);
+
+// }
+
+function llenarFrontPokemon(valor){
+   const pokemon = buscarPokemon(valor,data)
+
+
+    document.getElementById("Numero").innerHTML = "numero:"+pokemon.id
+    document.getElementById("Nombre").innerHTML = pokemon.name
+    document.getElementById("imagen").src = pokemon.img
+    document.getElementById("peso").innerHTML=pokemon.weight
+    
+    console.log(pokemon)
 }
 
 
