b1553c04df97301b0f4e873e82918b754364491b
marcelacoronel <marcelacoronelp@gmail.com>
2020-07-06 12:33:16 -0500

    nuevos cambios index y css Marce

--- test/data.spec.js
+++ test/data.spec.js
@@ -1,23 +1,23 @@
-import { example, anotherExample } from '../src/data.js';
+import { buscarPokemon } from '../src/data.js';
 
 
-describe('example', () => {
+describe('buscarPokemon', () => {
   it('is a function', () => {
-    expect(typeof example).toBe('function');
+    expect(typeof buscarPokemon).toBe('function');
   });
 
-  it('returns `example`', () => {
-    expect(example()).toBe('example');
+  it('returns `buscarPokemon`', () => {
+    expect(buscarPokemon()).toBe('buscarPokemon');
   });
 });
 
 
-describe('anotherExample', () => {
-  it('is a function', () => {
-    expect(typeof anotherExample).toBe('function');
-  });
+// describe('anotherExample', () => {
+//   it('is a function', () => {
+//     expect(typeof anotherExample).toBe('function');
+//   });
 
-  it('returns `anotherExample`', () => {
-    expect(anotherExample()).toBe('OMG');
-  });
-});
+//   it('returns `anotherExample`', () => {
+//     expect(anotherExample()).toBe('OMG');
+//   });
+// });
