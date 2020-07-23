3d79588702500e7e532761eeefae3b65d36a4a23
karlaguaita <66104641+karlaguaita@users.noreply.github.com>
2020-07-22 20:36:50 -0400

    cambio con los test

--- src/data.js
+++ src/data.js
@@ -2,24 +2,26 @@ import data from './data/pokemon/pokemon.js';
 export const buscarPokemon = (valor,arreglo) => {
 //  console.log(arreglo)
   // const arreglo = data.pokemon // Guarde la data del arreglo de pokemon
-
+  if (!valor)
+    return false
 
   // Se define la constante pokemon para el resultado de la busqueda y luego se busca en el arreglo con la propiedad find
   const pokemon2 = arreglo.find(pokemon2 => pokemon2.name.toLowerCase() == valor.toLowerCase() || pokemon2.id == valor)
+  //console.log(pokemon2)
   return pokemon2
 };
 
 
 export const detalleEvoluciones = (nombre, arregloData) =>{
-  console.log(nombre)
   const evolucion=arregloData.find(elemento=>elemento.name==nombre ||elemento.num==nombre ||elemento.img==nombre);
-  console.log(evolucion)
+  // console.log(evolucion)
   return evolucion;
   
 }
 
 export const candyPokemon = (arregloData,numero)=>{
   const candy=arregloData.find(elemento=>elemento.num==numero)
+  // console.log(candy)
   return candy;
 }
 
@@ -28,18 +30,13 @@ export const candyPokemon = (arregloData,numero)=>{
 export const ordenarPokemon = (arregloData)=>{
 
   const ordenarData=arregloData.sort((prev,next) => {
-   const ordenar = prev.name > next.name ? 1: -1;
- 
+    const ordenar = prev.name > next.name ? 1: -1;
     return ordenar;
   });
 
   return ordenarData;
   
 }
-
-
-
-// }
 
 // Retorna un entero aleatorio entre min (incluido) y max (excluido)
 export function getRandomInt(min, max) {
@@ -50,23 +47,20 @@ export function getRandomInt(min, max) {
 export const randomPokemon = () => {
 
     let numeroPokemon= getRandomInt(1,151)
-    console.log(numeroPokemon)
+    // console.log(numeroPokemon)
    const resultados = data.pokemon.find(pokemon => pokemon.id == numeroPokemon)
 
-   console.log(resultados) 
+  //  console.log(resultados) 
    return resultados
-
-  
 }
 
 
 export const probabilidadCombate = (numeroPokemon,numeroPokemonRival) =>{
-
   const tuPokemon = buscarPokemon(numeroPokemon.toString(), data.pokemon)
-  console.log(tuPokemon)
+  // console.log(tuPokemon)
 
   const pokemonRival = buscarPokemon(numeroPokemonRival.toString(),data.pokemon)
-  console.log(pokemonRival)
+  // console.log(pokemonRival)
   let total=0
   // estos for comparan las fortalezas de tu pokemon con las debilidades del rival en caso de conseguirla
   // suman 1
@@ -92,6 +86,6 @@ export const probabilidadCombate = (numeroPokemon,numeroPokemonRival) =>{
     }
     
   }
- console.log(total)  
+//  console.log(total)  
  return total
 }
